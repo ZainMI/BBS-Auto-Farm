f0964c170148990ea4a5c94c50d5bb7b7b80b7cb
Zain Magdon-Ismail <84550903+ZainMI@users.noreply.github.com>
2023-08-27 01:18:37 -0400

    Update auto.py

--- auto.py
+++ auto.py
@@ -4,10 +4,10 @@
 
 import pyautogui
 
-global START_QUEST = (1636,888)
-global RETRY =  (608,916)
-global RANDOM_CLICK = (1547,893)
-global TOP_OF_CLOSE = (1214,850)
-global PURCHASE_TICKETS = 
+START_QUEST = (1636,888)
+RETRY =  (608,916)
+RANDOM_CLICK = (1547,893)
+TOP_OF_CLOSE = (1214,850)
+PURCHASE_TICKETS = 
 
 def pointEvent():
