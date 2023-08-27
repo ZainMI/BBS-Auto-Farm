368197688ad33e38e93fa2afd04fe8f23e9583a2
Zain Magdon-Ismail <84550903+ZainMI@users.noreply.github.com>
2023-08-27 01:17:08 -0400

    Create auto.py

---
+++ auto.py
@@ -0,0 +1,13 @@
+#Place BBS in top right corner of screen and edit the coordinates below
+
+
+
+import pyautogui
+
+global START_QUEST = (1636,888)
+global RETRY =  (608,916)
+global RANDOM_CLICK = (1547,893)
+global TOP_OF_CLOSE = (1214,850)
+global PURCHASE_TICKETS = 
+
+def pointEvent():
