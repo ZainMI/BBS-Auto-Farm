fd23bb33b676a7058cce5e336f2444fce7272ce8
Zain Magdon-Ismail <84550903+ZainMI@users.noreply.github.com>
2023-08-28 01:33:11 -0400

    FARM

--- auto.py
+++ auto.py
@@ -1,13 +1,50 @@
-#Place BBS in top right corner of screen and edit the coordinates below
-
-
-
 import pyautogui
 
-START_QUEST = (1636,888)
-RETRY =  (608,916)
-RANDOM_CLICK = (1547,893)
-TOP_OF_CLOSE = (1214,850)
-PURCHASE_TICKETS = 
 
-def pointEvent():
+def farm(max_orbs, cont):
+    orbs_used = 0
+    while True:
+        start_quest = pyautogui.locateOnScreen("start_quest.png", confidence=0.8)
+        retry = pyautogui.locateOnScreen("retry.png", confidence=0.8)
+        tap_screen = pyautogui.locateOnScreen("tap_screen.png", confidence=0.8)
+        close = pyautogui.locateOnScreen("close.png", confidence=0.8)
+        purchase = pyautogui.locateOnScreen("purchase.png", confidence=0.8)
+        result_screen = pyautogui.locateOnScreen("result_screen.png", confidence=0.8)
+        continues = pyautogui.locateOnScreen("continue.png", confidence=0.8)
+        buying = pyautogui.locateOnScreen("buying.png", confidence=0.8)
+        if purchase != None and buying != None:
+            if orbs_used + 5 > max_orbs:
+                return "Finished"
+            orbs_used += 5
+            purchase = pyautogui.center(purchase)
+            pyautogui.click(purchase)
+        elif start_quest != None:
+            start_quest = pyautogui.center(start_quest)
+            pyautogui.click(start_quest)
+        elif retry != None:
+            retry = pyautogui.center(retry)
+            pyautogui.click(retry)
+        elif tap_screen != None:
+            tap_screen = pyautogui.center(tap_screen)
+            pyautogui.click(tap_screen)
+        elif close != None:
+            close = pyautogui.center(close)
+            pyautogui.click(close)
+        elif result_screen != None:
+            result_screen = pyautogui.center(result_screen)
+            pyautogui.click(result_screen)
+        elif continues != None and cont == "y":
+            continues = pyautogui.center(continues)
+            pyautogui.click(continues)
+
+
+quest = "this is placement text"
+while quest == "this is placement text":
+    print("Welcome to BBS Auto Farm!\nPress enter to continue")
+    quest = input()
+
+print("What is the maximum amount of orbs you would like to spend?")
+max_orbs = int(input("Enter max number of orbs: "))
+cont = input("Would you like to use revival candles? (y/n): ")
+print("Starting BBS Auto Farm!")
+print(farm(max_orbs, cont))
